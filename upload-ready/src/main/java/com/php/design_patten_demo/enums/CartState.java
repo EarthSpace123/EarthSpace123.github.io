c8f22045a397b10647fca09371f90b02fb199faf
EarthSpace123 <168200859+EarthSpace123@users.noreply.github.com>
2026-02-08 16:45:47 +0800

    Delete upload-ready directory

--- upload-ready/src/main/java/com/php/design_patten_demo/enums/CartState.java
+++
@@ -1,7 +0,0 @@
-package com.php.design_patten_demo.enums;
-
-public enum CartState {
-    EMPTY,
-    HAS_ITEMS,
-    CHECKED_OUT
-}
--- upload-ready/src/main/java/com/php/design_patten_demo/pattern/observer/OrderObserver.java
+++
@@ -1,5 +0,0 @@
-package com.php.design_patten_demo.pattern.observer;
-
-public interface OrderObserver {
-    void update(String orderNo, String message);
-}
